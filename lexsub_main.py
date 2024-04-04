37c95f8a85903ff33275bb673acb1be1d86b4275
ritiksundaram <80601685+ritiksundaram@users.noreply.github.com>
2024-04-03 20:40:51 -0400

    Update lexsub_main.py

--- lexsub_main.py
+++ lexsub_main.py
@@ -1,106 +1,69 @@
 #!/usr/bin/env python
 import sys
+import string
+from collections import Counter
+from typing import List
 
-from lexsub_xml import read_lexsub_xml
-from lexsub_xml import Context 
-
-# suggested imports 
 from nltk.corpus import wordnet as wn
 from nltk.corpus import stopwords
-
 import numpy as np
 import tensorflow
-
 import gensim
-import transformers 
+import transformers
 
-from typing import List
+from lexsub_xml import read_lexsub_xml
+from lexsub_xml import Context
 
 def tokenize(s): 
     """
-    a naive tokenizer that splits on punctuation and whitespaces.  
+    A naive tokenizer that splits on punctuation and whitespaces.  
     """
     s = "".join(" " if x in string.punctuation else x for x in s.lower())    
     return s.split() 
 
 def get_candidates(lemma, pos) -> List[str]:
+    """
+    Retrieves synonyms for a given lemma and part of speech from WordNet.
+    """
     synonyms = []
-    lemmas = wn.synsets(lemma,pos)
-    for sys in lemmas:
-        for lemm in sys.lemma_appearance():
-            app = lemm.appearance()
-            app = app.replace('_', ' ')
-            synonyms.append(app)
+    for synset in wn.synsets(lemma, pos=pos):
+        for lemma in synset.lemmas():
+            synonym = lemma.name().replace('_', ' ')
+            if synonym not in synonyms:
+                synonyms.append(synonym)
     return synonyms
 
-def smurf_predictor(context : Context) -> str:
+def smurf_predictor(context: Context) -> str:
     """
-    suggest 'smurf' as a substitute for all words.
+    Suggests 'smurf' as a substitute for all words.
     """
     return 'smurf'
 
-def wn_frequency_predictor(context : Context) -> str:
+def wn_frequency_predictor(context: Context) -> str:
+    """
+    Suggests a substitute for a word based on its frequency of appearance in WordNet.
+    """
     lemma = context.lemma
-    synonyms = []
-    lemmas = wn.synsets(lemma,pos)
     pos = context.pos
-    occ = []
-    occ = wn.synsets(lemma,pos)
-
-    for sys in lemmas:
-        for lemm in sys.lemma_appearance():
-            app = lemm.appearance()
-            app = app.replace('_', ' ')
-            synonyms.append(app)
-            if app in synonyms:
-                occurence = synonyms.occurence(app)
-                occ[occurence] = occ[occurence]+lemm.count()
-            else:
-                synonyms.append(app)
-                occ.append(lemm.count())
-    highest = max(occ)
-    occurence = occ.occurence(highest)
-    return synonyms[occurence]
-def wn_simple_lesk_predictor(context : Context) -> str:
-    # not completed
-    return None        
-   
-
-class Word2VecSubst(object):
-        
-    def __init__(self, filename):
-        self.model = gensim.models.KeyedVectors.load_word2vec_format(filename, binary=True)    
-
-    def predict_nearest(self,context : Context) -> str:
-            similar = []
-            ret = []
-            ret = get_candidates(context.lemma, context.pos)
-            for i in ret:
-                try: 
-                    similar[i] = self.model.similarity(i, context.lemma)
-            return list(similar)
-
-
-class BertPredictor(object):
-
-    def __init__(self): 
-        self.tokenizer = transformers.DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
-        self.model = transformers.TFDistilBertForMaskedLM.from_pretrained('distilbert-base-uncased')
-
-    def predict(self, context : Context) -> str:
-        # not completed
-        return None 5
-
+    candidates = get_candidates(lemma, pos)
     
+    frequency_dict = Counter()
+    for synset in wn.synsets(lemma, pos):
+        for lemma in synset.lemmas():
+            if lemma.name().replace('_', ' ') in candidates:
+                frequency_dict[lemma.name().replace('_', ' ')] += lemma.count()
+    
+    if frequency_dict:
+        return frequency_dict.most_common(1)[0][0]
+    else:
+        return lemma
 
-if __name__=="__main__":
+# Add additional functionality as needed here, such as evaluation methods or integration with embeddings
 
-    # At submission time, this program should run your best predictor (part 6).
+if __name__ == "__main__":
+    # Example usage
+    context = Context(lemma='happy', pos='a', left_context='I am very', right_context='today.')
+    print("Smurf Predictor:", smurf_predictor(context))
+    print("WordNet Frequency Predictor:", wn_frequency_predictor(context))
 
-    #W2VMODEL_FILENAME = 'GoogleNews-vectors-negative300.bin.gz'
-    #predictor = Word2VecSubst(W2VMODEL_FILENAME)
-
-    for context in read_lexsub_xml(sys.argv[1]):
-        #print(context)  # useful for debugging
-        prediction = smurf_predictor(context) 
-        print("{}.{} {} :: {}".format(context.lemma, context.pos, context.cid, prediction))
+    # Load your dataset, process it, and use the above functions for lexical substitution
